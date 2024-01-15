114c28cbf0abe421683354e3f8987b8bf504b42d
unknown <aricin.rotarius@gmail.com>
2024-01-15 14:47:58 -0800

    Edit about me layout

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,21 +5,21 @@ function About() {
         <img
           src='aricin-portrait2.jpg'
           alt='Profile'
-          className='mb-3 w-28 h-28 sm:w-36 sm:h-36 rounded-full'
+          className='mb-3 w-28 h-28 sm:w-32 sm:h-32 rounded-full'
         />
         <div className='sm:pl-5'>
-          <h1 className='text-4xl sm:text-5xl text-white mb-2 font-semibold'>Aricin Rotarius</h1>
-          <p className='text-md sm:text-xl text-gray-300'>Full-Stack Software Engineer</p>
-          <p className='text-md sm:text-xl text-gray-300'>Recent Grad @ UW-Madison</p>
-          <div className='flex mt-3 space-x-3 justify-center sm:justify-start'> 
+          <h1 className='text-4xl sm:text-4xl text-white mb-2 font-semibold'>Aricin Rotarius</h1>
+          <p className='text-md sm:text-lg text-gray-300'>Full-Stack Software Engineer</p>
+          <p className='text-md sm:text-lg text-gray-300'>Recent Grad @ UW-Madison</p>
+          <div className='flex mt-3 sm:space-x-3 space-x-4 justify-center sm:justify-start'> 
             <a href='https://linkedin.com/in/aricin/' target='_blank' className=''>
-              <img src='linkedin.svg' alt='LinkedIn' className='w-8 h-8' />
+              <img src='linkedin.svg' alt='LinkedIn' className='w-10 h-10 fill-white' />
             </a>
             <a href='https://github.com/aricin' target='_blank' className=''>
-              <img src='github.svg' alt='GitHub' className='w-8 h-8' />
+              <img src='github.svg' alt='GitHub' className='w-10 h-10' />
             </a>
             <a href='https://twitter.com/aricin_' target='_blank' className=''>
-              <img src='x.svg' alt='X' className='w-8 h-8' />
+              <img src='x.svg' alt='X' className='w-10 h-10' />
             </a>
           </div>
         </div>
