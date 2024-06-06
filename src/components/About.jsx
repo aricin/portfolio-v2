e74de58786fe8f19e117ccaeacd71fcc99563287
aricin <aricin.rotarius@gmail.com>
2024-06-05 19:57:57 -0700

    Update about me setcion

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,6 @@ function About() {
         <div className='sm:pl-5'>
           <h1 className='text-4xl sm:text-4xl text-white mb-2 font-semibold'>Aricin Rotarius</h1>
           <p className='text-md sm:text-lg text-gray-300'>Full-Stack Software Engineer</p>
-          <p className='text-md sm:text-lg text-gray-300'>Recent Grad @ UW-Madison</p>
           <div className='flex mt-3 sm:space-x-3 space-x-4 justify-center sm:justify-start'> 
             <a href='https://linkedin.com/in/aricin/' target='_blank' className=''>
               <img src='linkedin.svg' alt='LinkedIn' className='w-10 h-10 fill-white' />
@@ -24,9 +23,6 @@ function About() {
           </div>
         </div>
       </div>
-      <p className='mt-10 text-gray-300 text-center sm:text-start'>
-        Hello! I&apos;m a recent graduate from the University of Wisconsin-Madison, where I earned a Bachelor of Science in Computer Science and a certificate in Entrepreneurship. I&apos;m currently looking for my next positon as a Software Engineer, bringing along my passion for innovation and building cool stuff.
-      </p>
       <a href='https://chat.openai.com/g/g-ldBGWxlD0-aricingpt' className='flex row mt-6 justify-center sm:justify-start' target='_blank'>
         &#128073;
         <p className=' text-gray-300 underline hover:text-white'>
