e88327d550cc3344ceb1f6f36f886b04aedf4f37
unknown <aricin.rotarius@gmail.com>
2024-06-05 22:00:52 -0700

    Update about me section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,11 @@
 function About() {
   return (
     <div className='xl:sticky 2xl:top-24 top-16 flex flex-col max-w-lg h-fit mx-auto p-5 pb-16'>
-      <div className='flex flex-col sm:flex-row sm:text-left items-center text-center'>
+      <div className='flex flex-col sm:flex-row sm:text-left items-top text-center'>
         <img
           src='aricin-portrait2.jpg'
           alt='Profile'
-          className='mb-3 w-28 h-28 sm:w-32 sm:h-32 rounded-full'
+          className='mb-3 mt-1 w-28 h-28 sm:w-32 sm:h-32 rounded-full'
         />
         <div className='sm:pl-5'>
           <h1 className='text-4xl sm:text-4xl text-white mb-2 font-semibold'>Aricin Rotarius</h1>
@@ -21,14 +21,14 @@ function About() {
               <img src='x.svg' alt='X' className='w-10 h-10' />
             </a>
           </div>
+          <a href='https://chat.openai.com/g/g-ldBGWxlD0-aricingpt' className='flex row mt-8 justify-center sm:justify-start' target='_blank'>
+            &#128073;
+            <p className=' text-gray-300 underline hover:text-white'>
+              Ask me anything with AricinGPT
+            </p>
+          </a>
         </div>
       </div>
-      <a href='https://chat.openai.com/g/g-ldBGWxlD0-aricingpt' className='flex row mt-6 justify-center sm:justify-start' target='_blank'>
-        &#128073;
-        <p className=' text-gray-300 underline hover:text-white'>
-          Ask me anything with AricinGPT
-        </p>
-      </a>
     </div>
   );
 }
